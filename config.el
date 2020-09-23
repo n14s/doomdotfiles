2d8478a84ac3924487016a32a06af5abc6643218
n14s <n14x@mailbox.org>
2020-09-23 13:53:19 +0200

    fixed bug; still having problem with org-noter-insert-note

--- config.el
+++ config.el
@@ -252,16 +252,16 @@
   :commands (org-noter)
   :after (:any org pdf-view)
   :config
-  (setq
+;;  (setq
    ;; The WM can handle splits
-   org-noter-notes-window-location 'other-frame
+   ;; org-noter-notes-window-location 'other-frame
    ;; Please stop opening frames
-   org-noter-always-create-frame nil
+   ;; org-noter-always-create-frame nil
    ;; I want to see the whole file
-   org-noter-hide-other nil
+   ;; org-noter-hide-other nil
    ;; Everything is relative to the rclone mega
-   org-noter-notes-search-path (list org_notes)
-   )
+   ;; org-noter-notes-search-path (list org_notes)
+;;   )
   (add-hook! org-noter-notes-mode (require 'org-noter-pdftools))
   )
 
